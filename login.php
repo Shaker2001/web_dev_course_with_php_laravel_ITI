d44dafba78b4d90767fed909541f1ef3f4ae4d8f
Ahmed Shaker <74692808+Shaker2001@users.noreply.github.com>
2022-10-30 00:50:01 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+<title>login</title>
+</head>
+<body>
+    <center>  
+<h1>
+
+</h1>
+<form action="show.php" method="post" >
+
+<div>
+    <label for="email" >Email</label>
+    <input type="email" name="email" >
+</div>
+<div>
+<input type="checkbox" name="remember" value="=remember">remember
+</div>
+<label for="password">password</label> 
+<input type="password" name="password" id="password" />
+</div> <br />
+<div>
+<div>
+<input type="submit" value="submit" name="submit">
+</div> 
+
+<form >
+    </center>
+</body>
+</html>
